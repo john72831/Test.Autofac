39b302ffe1ecdc7233abd44332e1e3ca4fe3e9cf
John <john72831@hotmail.com>
2018-07-20 09:04:28 +0800

    加入專案檔案。

---
+++ Test.Autofac.ConsoleApp.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test.Autofac.ConsoleApp.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
